2531a24fc46b5317b698116a930c380f38d7bd16
hamza-1998 <126608088+hamza-1998@users.noreply.github.com>
2023-03-29 16:33:54 +0200

    Initial commit

---
+++ index.ts
@@ -0,0 +1,15 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+let nombre:Number;
+
+nombre=parseInt(prompt("ecrire un nombre"))
+console.log(nombre)
+
+if(nombre>0)nombre{
+  console.log("positive")
+}else{
+  console.log("negative")
+}
+console.log("le resultat est:" , nombre)
